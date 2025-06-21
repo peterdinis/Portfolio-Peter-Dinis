12ff69923946f018792a4dcc6bcb8ba52110cfe2
peterdinis <petter1@pobox.sk>
2025-06-21 21:04:18 +0200

    refactor: Knowledge for Expirience

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { ContactSection } from "@/components/home/ContactSection";
 import { HeroSection } from "@/components/home/HeroSection";
-import { ExperienceSection } from "@/components/knowledge/ExpirienceSection";
+import { ExperienceSection } from "@/components/expirience/ExpirienceSection";
 import { ProjectsSection } from "@/components/projects/ProjectSection";
 import { SkillsSection } from "@/components/skills/SkillsSection";
 import type { NextPage } from "next";
