680fbf5cb497f42008b2060f336a7b28e67e969c
peterdinis <petter1@pobox.sk>
2025-06-27 18:10:04 +0200

    Remove toaster component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ScrollToTop } from "@/components/shared/ScrollToTop";
-import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -71,7 +70,6 @@ export default function RootLayout({
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
 				{children}
-				<Toaster />
 				<ScrollToTop />
 			</body>
 		</html>
